57670f2e6ba2abe6c897a2a353229ed160de314a
Kürşat Şimşek <kursatsimsek@192.168.1.118>
2025-09-06 22:45:38 +0300

    Initial commit

---
+++ Modern-SwiftUI-Bootcamp-Task-5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Modern-SwiftUI-Bootcamp-Task-5
+//
+//  Created by Kürşat Şimşek on 6.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Modern-SwiftUI-Bootcamp-Task-5/Modern_SwiftUI_Bootcamp_Task_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Modern_SwiftUI_Bootcamp_Task_5App.swift
+//  Modern-SwiftUI-Bootcamp-Task-5
+//
+//  Created by Kürşat Şimşek on 6.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Modern_SwiftUI_Bootcamp_Task_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
